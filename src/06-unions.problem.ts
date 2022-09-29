6a3cf7d2301c48037522740f7ddeb31642981484
Pakapon Kaewmark <pakapon.k@skooldio.com>
2022-09-29 13:58:49 +0700

    :sparkles: Constraining Value Types

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -1,3 +1,4 @@
+type Role = "user" | "admin" | "super-admin";
 interface User {
   id: number;
   firstName: string;
@@ -8,7 +9,7 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: Role;
 }
 
 export const defaultUser: User = {
